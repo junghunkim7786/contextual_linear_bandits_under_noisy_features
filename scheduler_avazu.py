e7b4ec42c4885c9c018670fbfc39d1556278619f
MinchanJeong <minchanjn@gmail.com>
2023-02-27 08:56:51 +0000

    Update - Scheduler Argument T <- 100000

--- scheduler_avazu.py
+++ scheduler_avazu.py
@@ -6,7 +6,7 @@ os.environ["MKL_THREADING_LAYER"] = 'GNU'
 plotting_dict = {'cut':0, 'title':'Avazu CTR Dataset', 'y_lim':[0,0.11], 'y_ticks':[0, 0.05, 0.10], 'y_ticklabels':['0', '0.05' ,'0.10']}
 now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 seed_list = [12345, 23456, 34567, 45678, 56789, 67890, 78901, 89012, 90123, 1234]
-base_arg = ' --T 10000 --env avazu --resultfoldertail _ctr_avazu_{} '.format(now)
+base_arg = ' --T 100000 --env avazu --resultfoldertail _ctr_avazu_{} '.format(now)
 
 if os.path.exists('./real_datasets/avazu/preprocess/X0_avazu.npy'):
     print('Skip Preprocessing')
--- scheduler_ml100k.py
+++ scheduler_ml100k.py
@@ -6,7 +6,7 @@ os.environ["MKL_THREADING_LAYER"] = 'GNU'
 plotting_dict = {'cut':0, 'title':'MovieLens 100K Dataset', 'y_lim':[0,0.13], 'y_ticks':[0, 0.05, 0.10], 'y_ticklabels':['0', '0.05' ,'0.10']}
 now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 seed_list = [12345, 23456, 34567, 45678, 56789, 67890, 78901, 89012, 90123, 1234]
-base_arg = ' --T 10000 --env ml100k --resultfoldertail _ctr_ml100k_{} '.format(now)
+base_arg = ' --T 100000 --env ml100k --resultfoldertail _ctr_ml100k_{} '.format(now)
 
 if os.path.exists('./real_datasets/ml100k/preprocess/X0_ml100k.npy'):
     print('Skip Preprocessing')
